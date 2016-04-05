43c8e2830c708e8ee3f93170045e41712cb16afa
ROSA <gojiradam@gmail.com>
2016-04-05 21:32:54 +0200

    Change color test

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,7 +1,37 @@
 package sample;
 
+import javafx.animation.FillTransition;
+import javafx.animation.Timeline;
+import javafx.animation.TranslateTransition;
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Controller {
 
+    Main Root;
+
+    Controller(Pane rooter){
+        rooter = rooter;
+    }
+
+
+    public void animateLogo() {
+        Rectangle rect = new Rectangle (366, 366, 366, 366);
+        FillTransition ft = new FillTransition(Duration.millis(1000), rect, Color.RED, Color.BLACK);
+        ft.setCycleCount(400);
+        ft.setAutoReverse(true);
+        ft.play();
+        rooter.root.getChildren().add(ft.getShape());
+
+        ft.play();
+
+    }
 }
+
+
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,21 +1,26 @@
 package sample;
 
+import javafx.animation.FillTransition;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
+import javafx.util.Duration;
 
 public class Main extends Application {
 
     public Pane root = new Pane();
-    public Thread t1 = new Thread();
+    Controller controller = new Controller();
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -23,6 +28,14 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 600, 800));
         primaryStage.show();
+
+        Rectangle rect = new Rectangle (366, 366, 366, 366);
+        FillTransition ft = new FillTransition(Duration.millis(1000), rect, Color.RED, Color.BLACK);
+        ft.setCycleCount(400);
+        ft.setAutoReverse(true);
+        ft.play();
+        root.getChildren().add(ft.getShape());
+
     }
 
 
