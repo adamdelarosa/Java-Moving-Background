80470e8a101bcb6e288cbebc1ab65c7f307b4c76
ROSA <gojiradam@gmail.com>
2016-04-06 21:10:03 +0200

    Change color test

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,17 +1,5 @@
 package sample;
 
-import javafx.animation.FillTransition;
-import javafx.animation.Timeline;
-import javafx.animation.TranslateTransition;
-import javafx.application.Application;
-import javafx.fxml.FXML;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.stage.Stage;
-import javafx.util.Duration;
-
 public class Controller {
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,50 +1,33 @@
 package sample;
 
-import javafx.animation.FillTransition;
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Polygon;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.util.Duration;
 
 public class Main extends Application {
 
 
+
     @Override
     public void start(Stage stage) throws Exception {
-        Group root = new Group();
-        Scene scene = new Scene(root, 260, 80);
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        root.setStyle("-fx-background-color: #F0591E;");
+
+        Scene scene = new Scene(root);
+
+        stage.setTitle("FXErcycton v1.0.0");
+
         stage.setScene(scene);
-
-        Group g = new Group();
-
-        Polygon polygon = new Polygon();
-        polygon.getPoints().addAll(new Double[]{
-                0.0, 0.0,
-                20.0, 10.0,
-                10.0, 20.0 });
-
-        g.getChildren().add(polygon);
-
-        scene.setRoot(g);
         stage.show();
     }
 
 
 
     public void change() {
-        //root.setBackground(new Background(new BackgroundFill(Color.BLACK, null, null)));
+       // root.setBackground(new Background(new BackgroundFill(Color.BLACK, null, null)));
+
     }
 
 
