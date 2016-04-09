52ad61778a773cbce4717890af0a81b266016c57
ROSA <gojiradam@gmail.com>
2016-04-09 20:22:14 +0200

    Background class

--- src/sample/Background.java
+++ src/sample/Background.java
@@ -14,30 +14,26 @@ public class Background {
         ImageView imageViewOne = new ImageView();
         imageViewOne.setImage(backGroundimageSpace);
 
-            Thread backgroundScrollRunner = new Thread(new Runnable() {
-                public void run() {
+            Thread backgroundScrollRunner = new Thread(() -> {
 
-                    // code goes here.
-            for (moveX = 0; moveX <= 100; moveX++) {
-                try {
-                    Thread.sleep(400);
-                }catch (InterruptedException ie){
-                    System.out.println(ie + "FAIL.");
-                }
-                System.out.println(moveX);
-                if (moveX == 100) {
-                    moveX = 0;
+        for (moveX = 0; moveX <= 1000; moveX++) {
+            try {
+                Thread.sleep(10);
+                imageViewOne.setFitHeight(moveX);
 
-                }
-
+            }catch (InterruptedException ie){
+                System.out.println(ie + "FAIL.");
+            }
+            System.out.println(moveX);
+            if (moveX == 1000) {
+                moveX = 0;
             }
 
-                }
+        }
 
             });
         backgroundScrollRunner.start();
 
-        imageViewOne.setFitHeight(moveX);
 
         main.getBox().getChildren().add(imageViewOne);
         main.getRoot().getChildren().add(main.getBox());
