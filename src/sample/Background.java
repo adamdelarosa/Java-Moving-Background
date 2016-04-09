9d4f3adada7363afa94e3ecdde41c1facf8b3200
ROSA <gojiradam@gmail.com>
2016-04-09 20:19:07 +0200

    Background class

--- src/sample/Background.java
+++ src/sample/Background.java
@@ -8,22 +8,40 @@ public class Background {
 
     Main main;
     Image backGroundimageSpace = new Image("sample/asset/splash.jpg");
-    int x=30;
-    int y=30;
+    int moveX = 8000;
 
     public void changeBackground() {
         ImageView imageViewOne = new ImageView();
         imageViewOne.setImage(backGroundimageSpace);
+
+            Thread backgroundScrollRunner = new Thread(new Runnable() {
+                public void run() {
+
+                    // code goes here.
+            for (moveX = 0; moveX <= 100; moveX++) {
+                try {
+                    Thread.sleep(400);
+                }catch (InterruptedException ie){
+                    System.out.println(ie + "FAIL.");
+                }
+                System.out.println(moveX);
+                if (moveX == 100) {
+                    moveX = 0;
+
+                }
+
+            }
+
+                }
+
+            });
+        backgroundScrollRunner.start();
+
+        imageViewOne.setFitHeight(moveX);
+
         main.getBox().getChildren().add(imageViewOne);
         main.getRoot().getChildren().add(main.getBox());
 
 
-        for(int moveX=0;moveX<=100;moveX++){
-            System.out.println(moveX);
-            if (moveX == 100){
-                moveX = 0;
-            }
-
-        }
     }
 }
