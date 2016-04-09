982fea93cb9962f305935413aea9324110bafcca
ROSA <gojiradam@gmail.com>
2016-04-09 19:58:25 +0200

    Background class

--- src/sample/Background.java
+++ src/sample/Background.java
@@ -1,6 +1,5 @@
 package sample;
 
-
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
@@ -8,14 +7,10 @@ public class Background {
 
     Main main;
     Image backGroundimageSpace = new Image("sample/asset/splash.jpg");
-
     int x=30;
     int y=30;
 
-
     public void changeBackground() {
-
-
         ImageView imageViewOne = new ImageView();
         imageViewOne.setImage(backGroundimageSpace);
         main.getBox().getChildren().add(imageViewOne);
