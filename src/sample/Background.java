2643b257c18d17157c462b559841e16e353fe12e
ROSA <gojiradam@gmail.com>
2016-04-12 22:18:25 +0200

    Background class

--- src/sample/Background.java
+++ src/sample/Background.java
@@ -1,60 +1,40 @@
 package sample;
 
-import javafx.scene.Group;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.shape.*;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-
-import java.awt.*;
-import java.awt.Polygon;
-import java.awt.image.BufferedImage;
 import java.util.Random;
-
 
 public class Background {
 
     Main main;
-    private static final int STAR_COUNT = 20000;
-
-    public static HBox boxOne = new HBox();
-    Random rndmStar = new Random();
-    private final Rectangle[] nodes = new Rectangle[STAR_COUNT];
-
-
-
+    int i = 0;
+    HBox boxOne = new HBox();
+    Random rndmStarX = new Random();
     Thread backgroundScrollRunner = new Thread();
 
-
     public void changeBackground() {
-
+        int xPoz = rndmStarX.nextInt(450);
         Circle circle = new Circle();
-
         circle.setRadius(1);
-        circle.setFill(Color.WHITE);
+        circle.setFill(Color.BLUE);
 
         backgroundScrollRunner = new Thread(() -> {
-
-            for(int yPoz=rndmStar.nextInt(100);1<=2;yPoz+=rndmStar.nextInt(10)){
+            for(int yPoz=0;1<=2;yPoz+=2){
                 try {
+
+                    boxOne.setLayoutX(xPoz);
                     boxOne.setLayoutY(yPoz);
-                    boxOne.setLayoutX(yPoz);
-
-                    Thread.sleep(50);
-
-                    circle.setCenterX(yPoz);
+                    circle.setCenterX(xPoz);
                     circle.setCenterY(yPoz);
+                    Thread.sleep(50);
 
                 } catch (InterruptedException ie) {
                     System.out.println(ie + "FAIL.");
                 }
-               // System.out.println(yPoz);
             }
-
         });
         backgroundScrollRunner.start();
-
         //Add to Root
         boxOne.getChildren().add(circle);
         main.getRoot().getChildren().add(boxOne);
