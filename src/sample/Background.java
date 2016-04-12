1988cf1e4a3eaf0cd51751cd3e04ada8849a70c2
ROSA <gojiradam@gmail.com>
2016-04-12 21:34:39 +0200

    Background class

--- src/sample/Background.java
+++ src/sample/Background.java
@@ -5,6 +5,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.shape.*;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 import java.awt.*;
 import java.awt.Polygon;
@@ -15,31 +16,30 @@ import java.util.Random;
 public class Background {
 
     Main main;
-    int moveX = 8000;
+    private static final int STAR_COUNT = 20000;
+
     public static HBox boxOne = new HBox();
     Random rndmStar = new Random();
+    private final Rectangle[] nodes = new Rectangle[STAR_COUNT];
+
 
 
     Thread backgroundScrollRunner = new Thread();
-
-    Polygon polyline = new Polygon();
 
 
     public void changeBackground() {
 
         Circle circle = new Circle();
-        circle.setCenterX(1);
-        circle.setCenterY(100);
+
         circle.setRadius(1);
         circle.setFill(Color.WHITE);
 
-
         backgroundScrollRunner = new Thread(() -> {
-            for(int yPoz=0;yPoz<=1000;yPoz+=10){
+
+            for(int yPoz=rndmStar.nextInt(100);1<=2;yPoz+=rndmStar.nextInt(10)){
                 try {
                     boxOne.setLayoutY(yPoz);
                     boxOne.setLayoutX(yPoz);
-
 
                     Thread.sleep(50);
 
@@ -50,14 +50,13 @@ public class Background {
                     System.out.println(ie + "FAIL.");
                 }
                // System.out.println(yPoz);
-
             }
 
         });
         backgroundScrollRunner.start();
+
+        //Add to Root
         boxOne.getChildren().add(circle);
         main.getRoot().getChildren().add(boxOne);
-
-
     }
 }
