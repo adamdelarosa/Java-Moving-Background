d36a5776a5422cf7804a227656e3bad48355579c
ROSA <gojiradam@gmail.com>
2016-04-13 23:28:05 +0200

    Background class

--- src/sample/Background.java
+++ src/sample/Background.java
@@ -30,7 +30,9 @@ public class Background {
             }
         });
         backgroundScrollRunner.start();
+
         //Add to Root
+
         boxOne.getChildren().add(circle);
         main.getRoot().getChildren().add(boxOne);
     }
