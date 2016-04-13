1ed7d8ef42163aa269e7ed6aadc08b3e531980a8
ROSA <gojiradam@gmail.com>
2016-04-13 23:24:10 +0200

    Background class

--- src/sample/Background.java
+++ src/sample/Background.java
@@ -8,31 +8,29 @@ import java.util.Random;
 public class Background {
 
     Main main;
-    int i = 0;
-    HBox boxOne = new HBox();
-    Random rndmStarX = new Random();
-    Thread backgroundScrollRunner = new Thread();
+    public HBox boxOne = new HBox();
+    private Random rndmStarX = new Random();
+    private Thread backgroundScrollRunner = new Thread();
+    private int xPoz = rndmStarX.nextInt(450);
 
     public void changeBackground() {
-        int xPoz = rndmStarX.nextInt(450);
-        Circle circle = new Circle();
-        circle.setRadius(1);
-        circle.setFill(Color.BLUE);
+            Circle circle = new Circle();
+            circle.setRadius(1);
+            circle.setFill(Color.WHITE);
 
         backgroundScrollRunner = new Thread(() -> {
             for(int yPoz=0;1<=2;yPoz+=2){
                 try {
-
                     boxOne.setLayoutX(xPoz);
                     boxOne.setLayoutY(yPoz);
                     circle.setCenterX(xPoz);
                     circle.setCenterY(yPoz);
                     Thread.sleep(50);
-
                 } catch (InterruptedException ie) {}
             }
         });
         backgroundScrollRunner.start();
+
         //Add to Root
         boxOne.getChildren().add(circle);
         main.getRoot().getChildren().add(boxOne);
