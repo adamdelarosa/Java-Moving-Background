938d5921761fd3dd7857df9dfef791899e85f744
ROSA <gojiradam@gmail.com>
2016-04-18 21:43:51 +0200

    Background class

--- src/sample/BackgroundSet.java
+++ src/sample/BackgroundSet.java
@@ -1,5 +1,6 @@
 package sample;
 
+import javafx.geometry.Rectangle2D;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
@@ -10,8 +11,7 @@ public class BackgroundSet {
     Image image = new Image("sample/asset/splash.jpg");
     ImageView iv1 = new ImageView();
     private Thread backgroundScrollRunner = new Thread();
-
-
+    Rectangle2D viewportRect = new Rectangle2D(500, 500, 200, 200);
 
     BackgroundSet(){
         paintComponent();
@@ -23,8 +23,9 @@ public class BackgroundSet {
         iv1.setPreserveRatio(true);
         iv1.setSmooth(true);
         iv1.setCache(true);
-        iv1.setFitWidth(600);
-        iv1.setFitHeight(600);
+        iv1.setY(800);
+        iv1.setX(800);
+        iv1.setViewport(viewportRect);
 
         //Background movement:
         backgroundScrollRunner = new Thread(() -> {
