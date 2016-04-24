841bea728605fd05230a6aa6bc5ed75e3eed10e7
ROSA <gojiradam@gmail.com>
2016-04-24 19:33:37 +0200

    SpaceShip

--- src/sample/SpaceShip.java
+++ src/sample/SpaceShip.java
@@ -1,6 +1,9 @@
 package sample;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 /**
@@ -8,12 +11,32 @@ import javafx.scene.shape.Rectangle;
  */
 public class SpaceShip {
 
-    private final Rectangle hyperSpaceShip = new Rectangle();
+    Main main;
+    private Rectangle hyperSpaceShip;
+    final ImageView selectedImage = new ImageView();
+    Image assetSpaceShip = new Image(Main.class.getResourceAsStream("sample/asset/Ship.png"));
+
+
+
+    public SpaceShip(){
+        shipMovment();
+        addToScreen();
+    }
+
 
     public void shipMovment(){
+        hyperSpaceShip = new Rectangle(50,50, Color.BLUE);
+        hyperSpaceShip.setLayoutX(100);
+        hyperSpaceShip.setLayoutY(100);
 
 
+
+    }
+
+    public void addToScreen(){
         HBox boxForShip = new HBox();
+        boxForShip.getChildren().addAll(hyperSpaceShip);
+        main.getRoot().getChildren().add(boxForShip);
     }
 
 }
